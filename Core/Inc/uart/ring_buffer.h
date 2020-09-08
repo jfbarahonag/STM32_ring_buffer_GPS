27f1f88c3fe588ac1d0fd8df1028c6699d4f66a1
Juan F Barahona G <felipe.barahona@titoma.com>
2020-09-07 21:21:26 -0500

    Project started

---
+++ Core/Inc/uart/ring_buffer.h
@@ -0,0 +1,13 @@
+/*
+ * ring_buffer.h
+ *
+ *  Created on: Sep 7, 2020
+ *      Author: felipebg
+ */
+
+#ifndef INC_UART_RING_BUFFER_H_
+#define INC_UART_RING_BUFFER_H_
+
+
+
+#endif /* INC_UART_RING_BUFFER_H_ */
---
+++ Core/Src/uart/ring_buffer.c
@@ -0,0 +1,6 @@
+/*
+ * ring_buffer.c
+ *
+ *  Created on: Sep 7, 2020
+ *      Author: felipebg
+ */
